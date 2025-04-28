84a45a4a2a4502ab05623e9c98fb853b5df5fe6b
Jonas <jonascamargo999@gmail.com>
2025-04-28 20:45:41 -0300

    feat: implementa serviço de resolução de problemas de programação linear com formulário UI

--- ui/src/app/services/solver.service.ts
+++ ui/src/app/services/solver.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+interface SolverRequest {
+  objective: string;
+  objective_func: number[];
+  constraints: { coefficients: number[]; operator: string; valor: number }[];
+}
+
+interface SolverResponse {
+  variaveis: { [key: string]: number };
+  valor_otimo: number;
+  status: string;
+  error?: string;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class SolverService {
+  private apiUrl = 'http://127.0.0.1:8000/api/solve/'; // Use a URL do seu backend
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  solveProblem(problemData: SolverRequest): Observable<SolverResponse> {
+    return this.http.post<SolverResponse>(this.apiUrl, problemData);
+  }
 }
