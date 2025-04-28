7bcd76c5c3baa39ffa67be1b1fd1526714f1d53d
Jonas <jonascamargo999@gmail.com>
2025-04-28 20:31:15 -0300

    feat: adiciona angular material e plotly.js

--- ui/src/app/app.component.ts
+++ ui/src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SolverComponent } from './components/solver/solver.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [
+    RouterOutlet,
+    SolverComponent
+  ],
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
   title = 'ui';
--- ui/src/app/components/solver/solver.component.ts
+++ ui/src/app/components/solver/solver.component.ts
@@ -1,11 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ElementRef, ViewChild } from '@angular/core';
+import Plotly from 'plotly.js-dist-min';
+import { Data, Layout, Config } from 'plotly.js-dist-min';
+
+interface Graph {
+  data: Partial<Data>[];
+  layout: Partial<Layout>;
+  config?: Partial<Config>;
+}
 
 @Component({
   selector: 'app-solver',
+  standalone: true,
   imports: [],
-  templateUrl: './solver.component.html',
-  styleUrl: './solver.component.css'
+  templateUrl: './solver.component.html'
 })
-export class SolverComponent {
+export class SolverComponent implements AfterViewInit{
+  @ViewChild('plotlyGraph') graphDiv!: ElementRef;
+
+  public graph: Graph = {
+    data: [
+      { x: [1, 2, 3], y: [2, 6, 3], type: 'scatter', marker: { color: 'red' } },
+      { x: [1, 2, 3], y: [2, 5, 3], type: 'bar' },
+    ],
+    layout: { width: 640, height: 480, title: 'Um Gráfico Fantástico' },
+    config: {}
+  };
+
+  ngAfterViewInit(): void {
+    this.plot();
+  }
+
+  plot(): void {
+    Plotly.newPlot(this.graphDiv.nativeElement, this.graph.data as Data[], this.graph.layout, this.graph.config);
+  }
 
 }
