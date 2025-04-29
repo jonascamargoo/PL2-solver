b63879dbbd73449a519f4e70f7a7ca0fe6237489
Jonas <jonascamargo999@gmail.com>
2025-04-28 22:44:47 -0300

    fix: conserta o solver.service

--- ui/src/app/services/solver.service.ts
+++ ui/src/app/services/solver.service.ts
@@ -1,47 +1,29 @@
-// import { Injectable } from '@angular/core';
-// import { HttpClient } from '@angular/common/http';
-// import { Observable } from 'rxjs';
-
-// interface SolverRequest {
-//   objective: string;
-//   objective_func: number[];
-//   constraints: { coefficients: number[]; operator: string; valor: number }[];
-// }
-
-// interface SolverResponse {
-//   variaveis: { [key: string]: number };
-//   valor_otimo: number;
-//   status: string;
-//   error?: string;
-// }
-
-// @Injectable({
-//   providedIn: 'root'
-// })
-// export class SolverService {
-//   private apiUrl = 'http://127.0.0.1:8000/api/solve/';
-
-//   constructor(private http: HttpClient) { }
-
-//   solveProblem(problemData: SolverRequest): Observable<SolverResponse> {
-//     return this.http.post<SolverResponse>(this.apiUrl, problemData);
-//   }
-// }
-
-
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+
+interface SolverRequest {
+  objective: string;
+  objective_func: number[];
+  constraints: { coefficients: number[]; operator: string; valor: number }[];
+}
+
+interface SolverResponse {
+  variaveis: { [key: string]: number };
+  valor_otimo: number;
+  status: string;
+  error?: string;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class SolverService {
-  private apiUrl = 'http://localhost:8000/api/solve/';
+  private apiUrl = 'http://127.0.0.1:8000/api/solve/';
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) { }
 
-  solve(problemData: any): Observable<any> {
-    return this.http.post(this.apiUrl, problemData);
+  solve(problemData: SolverRequest): Observable<SolverResponse> {
+    return this.http.post<SolverResponse>(this.apiUrl, problemData);
   }
 }
